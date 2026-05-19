a7a65ba3f89fa5e5b3b0151b947e221db142a86f
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2026-05-19 15:22:04 -0700

    add preview

--- dotnet/work-iq-teams-bot/work-iq-teams-bot.TeamsApp/Program.cs
+++ dotnet/work-iq-teams-bot/work-iq-teams-bot.TeamsApp/Program.cs
@@ -2,22 +2,16 @@
 // Licensed under the MIT License.
 
 using Microsoft.Teams.Apps;
-//using Microsoft.Teams.Apps.Diagnostics;
-//using Microsoft.Teams.Core.Diagnostics;
+using Microsoft.Teams.Apps.Diagnostics;
+using Microsoft.Teams.Core.Diagnostics;
 using work_iq_teams_bot.TeamsApp;
-
-
 
 WebApplicationBuilder builder = WebApplication.CreateSlimBuilder(args);
 IServiceProvider? rootServiceProvider = null;
 
-//builder.AddServiceDefaults(
-//    activitySources: [CoreTelemetryNames.ActivitySourceName, TeamsBotApplicationTelemetry.ActivitySourceName, "Experimental.Microsoft.Agents.AI", "ModelContextProtocol"],
-//    meterNames: [CoreTelemetryNames.MeterName, TeamsBotApplicationTelemetry.MeterName, "Experimental.Microsoft.Agents.AI", "ModelContextProtocol"]);
-
 builder.AddServiceDefaults(
-    activitySources: [ "Experimental.Microsoft.Agents.AI", "ModelContextProtocol"],
-    meterNames: [ "Experimental.Microsoft.Agents.AI", "ModelContextProtocol"],
+    activitySources: [CoreTelemetryNames.ActivitySourceName, TeamsBotApplicationTelemetry.ActivitySourceName, "Experimental.Microsoft.Agents.AI", "ModelContextProtocol"],
+    meterNames: [CoreTelemetryNames.MeterName, TeamsBotApplicationTelemetry.MeterName, "Experimental.Microsoft.Agents.AI", "ModelContextProtocol"],
     rootProviderAccessor: () => rootServiceProvider!);
 
 builder.Services
